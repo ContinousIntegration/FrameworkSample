2f8621b1b2d61e5dcc164ff6b6cd1e3477b9263d
School <fodeguipro@gmail.com>
2018-01-17 15:04:53 +0100

    gitifying

---
+++ FrameworkSample/Pair.swift
@@ -0,0 +1,17 @@
+//
+//  Pair.swift
+//  FrameworkSample
+//
+//  Created by School on 17/01/2018.
+//  Copyright © 2018 Fodé Guirassy. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    
+    public var isPAir: Bool {
+        return self % 2 == 0
+    }
+
+}
--- FrameworkSampleTests/FrameworkSampleTests.swift
+++ FrameworkSampleTests/FrameworkSampleTests.swift
@@ -11,26 +11,24 @@ import XCTest
 
 class FrameworkSampleTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testZero() {
+        XCTAssertEqual(0.isPAir, true)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testUn() {
+        XCTAssertEqual(1.isPAir, false)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTwo() {
+        XCTAssertEqual(2.isPAir, true)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAny() {
+        XCTAssertEqual(444.isPAir, true)
+    }
+    
+    func testNegatif() {
+        XCTAssertEqual((-1).isPAir, false)
     }
     
 }
