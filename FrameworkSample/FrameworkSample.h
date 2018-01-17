1bbcf64cc73bebee6aafbf1ba3c1192b8e211e19
School <fodeguipro@gmail.com>
2018-01-17 14:26:25 +0100

    Initial Commit

---
+++ FrameworkSample/FrameworkSample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkSample.h
+//  FrameworkSample
+//
+//  Created by School on 17/01/2018.
+//  Copyright © 2018 Fodé Guirassy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkSample.
+FOUNDATION_EXPORT double FrameworkSampleVersionNumber;
+
+//! Project version string for FrameworkSample.
+FOUNDATION_EXPORT const unsigned char FrameworkSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkSample/PublicHeader.h>
+
+
---
+++ FrameworkSampleTests/FrameworkSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  FrameworkSampleTests.swift
+//  FrameworkSampleTests
+//
+//  Created by School on 17/01/2018.
+//  Copyright © 2018 Fodé Guirassy. All rights reserved.
+//
+
+import XCTest
+@testable import FrameworkSample
+
+class FrameworkSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
